773df8a794a168b67e2946f8596c50d55348e3ba
topsneeraj <agnihotrineeraj00@gmail.com>
2020-10-26 18:57:01 +0530

    sdsd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule,Routes} from  '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegComponent } from './reg/reg.component';
+import { AddempComponent } from './addemp/addemp.component';
+import { ContinerComponent } from './continer/continer.component';
 
+const app :Routes = [{path:'',component:LoginComponent},{path:'login',component:LoginComponent},{path:'master',component:AppComponent,children:[{path:'addemp',component:AddempComponent}]},{path:'reg',component:RegComponent}]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegComponent,
+    AddempComponent,
+    ContinerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(app)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ContinerComponent]
 })
 export class AppModule { }
---
+++ src/app/continer/continer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-continer',
+  templateUrl: './continer.component.html',
+  styleUrls: ['./continer.component.css']
+})
+export class ContinerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
